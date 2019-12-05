5402f2020853a40cf16627626aa5f4cbcb87c521
Alex Bresler <abresler@asbcllc.com>
2019-12-05 16:38:15 -0500

    d

--- tests/testthat/test-fundmanager.R
+++ tests/testthat/test-fundmanager.R
@@ -3,11 +3,5 @@ test_that("data wrappers works", {
 
   testthat::skip_on_cran()
 
-  testthat::expect_that(get_data_cb_unicorns(), testthat::is_a("data.frame"))
-  testthat::expect_that(get_data_ycombinator_alumni(), testthat::is_a("data.frame"))
-  testthat::expect_that(get_data_sec_closed_end_funds(), testthat::is_a("data.frame"))
-  testthat::expect_that(get_data_sec_foia_requests(), testthat::is_a("data.frame"))
-  testthat::expect_that(get_data_finra_entities(entity_names = c("EJF", "Blackstone", "Next Play", "Simple Capital"), score_threshold = .25, ocr_pdf = TRUE),
-                        testthat::is_a("data.frame"))
-  testthat::expect_that(get_data_crsp_indicies_returns(), testthat::is_a("data.frame"))
+
 })
